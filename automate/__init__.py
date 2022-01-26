a9b10dfbd9f680b046b5359edd62929f58a4ab4f
Benjamin Jones <benjones@cs.washington.edu>
2022-01-25 19:55:21 -0800

    Added visualization functions for tensorboard.

--- automate/__init__.py
+++ automate/__init__.py
@@ -1,7 +1,7 @@
 from .conversions import jsonify, torchify
 from .brep import PartFeatures, part_to_graph, HetData, PartDataset
 from .sbgcn import SBGCN, LinearBlock
-from .uvgrid import UVPartDataModule, UVPartDataset, SimplePartDataset
+from .uvgrid import UVPartDataModule, UVPartDataset, SimplePartDataset, UVPredSBGCN, tb_comp, tb_mesh, tb_edge_mesh, tb_face_mesh, SimplePartDataModule
 
 __all__ = [
     'jsonify', 
@@ -14,5 +14,11 @@ __all__ = [
     'PartDataset',
     'UVPartDataModule',
     'UVPartDataset',
-    'SimplePartDataset'
+    'SimplePartDataset',
+    'UVPredSBGCN',
+    'tb_comp',
+    'tb_mesh',
+    'tb_edge_mesh',
+    'tb_face_mesh',
+    'SimplePartDataModule'
     ]
--- automate/uvgrid.py
+++ automate/uvgrid.py
@@ -5,27 +5,54 @@ from torch_scatter import scatter_mean
 from . import PartDataset, SBGCN, LinearBlock, part_to_graph
 from pspy import PartOptions, Part
 import os
+import numpy as np
 
-class SimplePartDataset(torch.utils.data.Dataset):
-    def __init__(self, path, n_samples = 10, normalize = True, mode='train'):
+class SimplePartDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        path, 
+        normalize = True,
+        batch_size = 32,
+        shuffle = True,
+        num_workers = 10,
+        persistent_workers = True
+    ):
         super().__init__()
         self.path = path
-        self.n_samples = n_samples
+        self.normalize = normalize
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
+        self.persistent_workers = persistent_workers and (self.num_workers > 0)
+
+
+    def setup(self, **kwargs):
+        self.train = SimplePartDataset(self.path, self.normalize, 'train')
+        self.test = SimplePartDataset(self.path, self.normalize, 'test')
+        self.validate = SimplePartDataset(self.path, self.normalize, 'validate')
+
+    def train_dataloader(self):
+        return tg.loader.DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers,shuffle=self.shuffle, persistent_workers=self.persistent_workers)
+
+    def val_dataloader(self):
+        return tg.loader.DataLoader(self.validate, batch_size=self.batch_size, num_workers=self.num_workers,shuffle=False, persistent_workers=self.persistent_workers)
+
+    def test_dataloader(self):
+        return tg.loader.DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers,shuffle=False, persistent_workers=self.persistent_workers)
+
+class SimplePartDataset(torch.utils.data.Dataset):
+    def __init__(self, path, normalize = True, mode='train'):
+        super().__init__()
+        self.path = os.path.join(path,mode)
         self.normalize = normalize
         self.mode = mode
-        self.files = [os.path.join(path,x) for x in os.listdir(path) if x.endswith('.pt')]
-        cutoff = int(len(self.files) * 0.95)
-        train_files = self.files[:cutoff]
-        val_files = self.files[cutoff:]
-        self.files = {
-            'train':train_files,
-            'validate':val_files
-        }
-    
+        self.files = [os.path.join(self.path,x) for x in os.listdir(self.path) if x.endswith('.pt')]
+            
     def __len__(self):
-        return len(self.files[self.mode])
+        return len(self.files)
+
     def __getitem__(self, idx):
-        data = torch.load(self.files[self.mode][idx])
+        data = torch.load(self.files[idx])
 
         # Normalization
         if self.normalize:
@@ -65,13 +92,6 @@ class SimplePartDataset(torch.utils.data.Dataset):
         if self.normalize:
             edge_xyz = (edge_xyz - center_point) / scale
         data.edge_samples = edge_xyz
-
-
-        #d_samps = data.face_samples.shape[-1]
-        #stride = d_samps // self.n_samples
-
-        #data.face_samples = data.face_samples.permute((0,2,3,1))[:,::stride,::stride,:]
-        #data.edge_samples = data.edge_samples.permute((0,2,1))[:,::stride,:]
 
         return data
 
@@ -248,3 +268,208 @@ class UVPartDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return tg.data.DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers,shuffle=False)
 
+
+class UVPredSBGCN(pl.LightningModule):
+    def __init__(self,
+        sbgcn_hidden_size = 128,
+        render_hidden_sizes = (1024, 1024),
+        pred_edges = True,
+        pred_faces = True,
+        sbgcn_k = 0
+    ):
+        super().__init__()
+
+        f_in = 17
+        l_in = 10
+        e_in = 15
+        v_in = 3
+
+        self.pred_edges = pred_edges
+        self.pred_faces = pred_faces
+        self.sbgcn_hidden_size = sbgcn_hidden_size
+        self.sbgcn_k = sbgcn_k
+        self.sbgcn = SBGCN(f_in, l_in, e_in, v_in, self.sbgcn_hidden_size, self.sbgcn_k)
+        self.render_hidden_sizes = render_hidden_sizes
+
+        if self.pred_edges:
+            self.edge_lin = LinearBlock(self.sbgcn_hidden_size, *self.render_hidden_sizes, 30)
+        if self.pred_faces:
+            self.surf_lin = LinearBlock(self.sbgcn_hidden_size, *self.render_hidden_sizes, 400)
+    
+    def forward(self, x):
+        #e = self.vert_to_edge((self.vert_in(x.vertices), self.edge_in(x.edges)), x.edge_to_vertex[[1,0],:])
+        #return torch.tanh(self.edge_conv(e))
+        x_t, x_p, x_f, x_l, x_e, x_v = self.sbgcn(x)
+        if self.pred_faces:
+            f_pred = torch.tanh(self.surf_lin(x_f))
+        if self.pred_edges:
+            e_pred = torch.tanh(self.edge_lin(x_e))
+        if self.pred_faces and self.pred_edges:
+            return e_pred, f_pred
+        elif self.pred_edges:
+            return e_pred
+        else:
+            return self.pred_faces
+    
+    def training_step(self, batch, batch_idx):
+        if self.pred_edges:
+            target_edge = batch.edge_samples.reshape(-1,30)
+        if self.pred_faces:
+            target_surf = batch.face_samples.reshape(-1,400)
+        if self.pred_faces and self.pred_edges:
+            pred_edge, pred_surf = self(batch)
+        elif self.pred_edges:
+            pred_edge = self(batch)
+        else:
+            pred_surf = self(batch)
+
+        if self.pred_edges:
+            loss_edge = torch.nn.functional.mse_loss(pred_edge, target_edge)
+            self.log('train_loss_edge', loss_edge)
+            loss = loss_edge
+        if self.pred_faces:
+            loss_surf = torch.nn.functional.mse_loss(pred_surf, target_surf)
+            self.log('train_loss_surf', loss_surf)
+            loss = loss_surf
+
+        if self.pred_faces and self.pred_edges:
+            loss = loss_edge + loss_surf
+        
+        self.log('train_loss',loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        if self.pred_edges:
+            target_edge = batch.edge_samples.reshape(-1,30)
+        if self.pred_faces:
+            target_surf = batch.face_samples.reshape(-1,400)
+        if self.pred_faces and self.pred_edges:
+            pred_edge, pred_surf = self(batch)
+        elif self.pred_edges:
+            pred_edge = self(batch)
+        else:
+            pred_surf = self(batch)
+
+        if self.pred_edges:
+            loss_edge = torch.nn.functional.mse_loss(pred_edge, target_edge)
+            self.log('val_loss_edge', loss_edge)
+            loss = loss_edge
+        if self.pred_faces:
+            loss_surf = torch.nn.functional.mse_loss(pred_surf, target_surf)
+            self.log('val_loss_surf', loss_surf)
+            loss = loss_surf
+
+        if self.pred_faces and self.pred_edges:
+            loss = loss_edge + loss_surf
+        
+        self.log('val_loss',loss)
+    
+    def test_step(self, batch, batch_idx):
+        if self.pred_edges:
+            target_edge = batch.edge_samples.reshape(-1,30)
+        if self.pred_faces:
+            target_surf = batch.face_samples.reshape(-1,400)
+        if self.pred_faces and self.pred_edges:
+            pred_edge, pred_surf = self(batch)
+        elif self.pred_edges:
+            pred_edge = self(batch)
+        else:
+            pred_surf = self(batch)
+
+        if self.pred_edges:
+            loss_edge = torch.nn.functional.mse_loss(pred_edge, target_edge)
+            self.log('test_loss_edge', loss_edge)
+            loss = loss_edge
+        if self.pred_faces:
+            loss_surf = torch.nn.functional.mse_loss(pred_surf, target_surf)
+            self.log('test_loss_surf', loss_surf)
+            loss = loss_surf
+
+        if self.pred_faces and self.pred_edges:
+            loss = loss_edge + loss_surf
+        
+        self.log('test_loss',loss)
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+# Tensorboard Visualization Helpers
+
+def tb_edge_mesh(edge_samples, color=(1.0, .8, .1), r = 0.001, M=3):
+    starts = edge_samples[:,:-1,:].reshape(-1,3)
+    ends = edge_samples[:,1:,:].reshape(-1,3)
+    N = starts.shape[0]
+    dirs = ends - starts
+    dirs = dirs / torch.linalg.norm(dirs,dim=1).reshape((-1,1))
+    x = torch.cross(dirs, torch.rand_like(dirs))
+    x = x / torch.linalg.norm(x,dim=1).reshape((-1,1))
+    y = torch.cross(dirs, x)
+    t = torch.arange(0,2*np.pi, 2*np.pi/M)
+    M = len(t)
+    sint = torch.sin(t)
+    cost = torch.cos(t)
+    circs = r * (torch.outer(cost, x.flatten()) + torch.outer(sint, y.flatten())).reshape(M,-1,3)
+    start_circs = (starts + circs).reshape(-1,3)
+    end_circs = (ends + circs).reshape(-1,3)
+    edge_verts = torch.cat([start_circs, end_circs],dim=0)
+    bottom_tris = torch.tensor([[m*N+n, ((m+1)%M)*N+n, ((m+1)%M)*N+n+N*M] for n in range(N) for m in range(M)])
+    top_tris = torch.tensor([[m*N+n, ((m+1)%M)*N+n+M*N, m*N+n+M*N] for n in range(N) for m in range(M)])
+    edge_faces = torch.cat([bottom_tris,top_tris],dim=0)
+
+    edge_verts = edge_verts
+    edge_faces = edge_faces
+    edge_colors = torch.tensor(color).tile((edge_verts.shape[0], 1))
+    return edge_verts, edge_faces, edge_colors
+
+def tb_face_mesh(face_samples, color=(.3,.7,.8), w = 0.001):
+    w = 0.001
+    xyz = face_samples.reshape(-1,4)[:,:3]
+    mask = face_samples.reshape(-1,4)[:,3]
+    K = len(mask)
+    cube_v = torch.tensor([
+        [-w,-w,-w],
+        [-w,-w,w],
+        [-w,w,-w],
+        [-w,w,w],
+        [w,-w,-w],
+        [w,-w,w],
+        [w,w,-w],
+        [w,w,w]
+    ])
+    cube_e = torch.tensor([
+        [0,2,1],[1,2,3],
+        [0,1,4],[1,5,4],
+        [1,3,5],[3,7,5],
+        [3,2,6],[3,6,7],
+        [2,0,4],[2,4,6],
+        [4,5,6],[5,7,6]
+    ])
+    cubes_v = (xyz.reshape(-1,1,3) + cube_v.reshape(1,8,3)).reshape(-1,3)
+    cubes_f = torch.cat([cube_e + 8*n for n in range(K)],dim=0)
+    cubes_m = mask.repeat_interleave(8).reshape(-1,1)
+    base_color = torch.tensor(color).tile((cubes_v.shape[0], 1))
+    white = torch.ones_like(base_color)
+    cubes_c = cubes_m * base_color + (1-cubes_m) * white
+
+    cubes_v = cubes_v
+    cubes_f = cubes_f
+    cubes_c = cubes_c
+
+    return cubes_v, cubes_f, cubes_c
+
+def tb_mesh(face_samples, edge_samples, color=(1.0, .8, .1), w=0.001, r=0.001, M=3):
+    e_v, e_f, e_c = tb_edge_mesh(edge_samples, color, r, M)
+    f_v, f_f, f_c = tb_face_mesh(face_samples, color, w)
+    v = torch.cat([e_v, f_v], dim=0)
+    f = torch.cat([e_f,f_f], dim=0)
+    c = torch.cat([e_c,f_c],dim=0)
+    return v, f, c
+    
+def tb_comp(face_samples, edge_samples, face_pred, edge_pred, gt_color=(.3,.7,.8), pred_color=(1.0, .8, .1), w=0.001, r=0.001, M=3):
+    v_gt, f_gt, c_gt = tb_mesh(face_samples, edge_samples, gt_color)
+    v_p, f_p, c_p = tb_mesh(face_pred, edge_pred, pred_color)
+    v = torch.cat([v_gt, v_p], dim=0)
+    f = torch.cat([f_gt, f_p], dim=0)
+    c = torch.cat([c_gt, c_p],dim=0)
+    return v, f, c
