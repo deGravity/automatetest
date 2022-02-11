2cafdc93690cc4f35eaf33d9a341bae1352f440d
James Noeckel <jamesn8@cs.washington.edu>
2022-02-10 22:36:01 -0800

    added test of failure case

--- pspy/main.cpp
+++ pspy/main.cpp
@@ -9,13 +9,14 @@ using namespace pspy;
 
 int main(int argc, char** argv) {
     PartOptions options;
-    options.onshape_style = false;
-    options.default_mcfs_only_face_axes = false;
-    options.num_uv_samples = 100;
-    options.collect_inferences = false;
-    options.default_mcfs = false;
+    //options.onshape_style = false;
+    //options.default_mcfs_only_face_axes = false;
+    //options.num_uv_samples = 100;
+    //options.collect_inferences = false;
+    //options.default_mcfs = false;
 
+    std::cout << "loading" << TEST_PART << std::endl;
     auto part = Part(TEST_PART, options);
-
+    std::cout << "valid: " << part._is_valid << std::endl;
     return 0;
 }
