2f53738c253ea84ea9d69eb8371ac122a8fd6b79
Ben Jones <bentodjones@gmail.com>
2022-04-12 01:03:40 -0700

    Fixed read_file to recognize full file text.

--- pspy/body.cpp
+++ pspy/body.cpp
@@ -10,6 +10,15 @@ static std::string get_extension(std::string path) {
 }
 
 std::vector<std::shared_ptr<Body>> read_file(std::string path) {
+
+    if (path.rfind("ISO-10303-21;", 0) != std::string::npos) {
+        return read_step(path);
+    }
+
+    if (path.rfind("**", 0) != std::string::npos) {
+        return read_xt(path);
+    }
+
     std::string ext = get_extension(path);
 
     if (ext == "x_t" || ext == "xt") {
