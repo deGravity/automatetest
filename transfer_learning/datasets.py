7e092cbb6e4d521d745aae6e58e718985f1a8dd0
Ben Jones <bentodjones@gmail.com>
2022-05-16 16:12:35 -0700

    Simplified datasets.

--- transfer_learning/datasets.py
+++ transfer_learning/datasets.py
@@ -6,7 +6,32 @@ import os
 import random
 import pickle
 from pspy import Part, ImplicitPart, PartOptions
+from automate import part_to_graph, PartFeatures, implicit_part_to_data
 from functools import cache
+
+'''
+Dataset structure:
+
+Low Level: Index + Zip or Directory -> Part Object / PyGeo + Labels
+Mid Level: Caching: In Memory and / or On Disk
+High Level: Subsetting - let us change subsets without losing cache
+
+Considerations:
+
+Mode: train and test have unique index set numbers, but only one should
+be subset or split for validation.
+
+Cache-Retention: It would be great to keep the in-memory cache between
+experiments, but this would require keeping the data loaders alive while
+somehow changing their underlying data sets.
+
+Labels: mfcad and fusion360seg have similar label structures. We could
+do the same for automate, or just load the labels into the index jsons
+on everything once and have a more consistent labeling structure. Labels
+also make validation sampling trickier since we want to do stratified
+sampling in these cases.
+
+'''
 
 class CachedDataset(Dataset):
     def __init__(self, ds, cache_dir = None, memcache = False):
@@ -14,6 +39,8 @@ class CachedDataset(Dataset):
         self.ds = ds
         self.memcache = memcache
         self.cache_dir = cache_dir
+        if hasattr(ds, 'labels'):
+            self.labels = ds.labels
 
     def __len__(self):
         return len(self.ds)
@@ -38,10 +65,18 @@ class CachedDataset(Dataset):
     
     def get_filecached(self, idx):
         if self.cache_dir:
-            cache_path = os.path.join(self.cache_dir, idx)
+            cache_path = os.path.join(self.cache_dir, f'{idx}.pt')
             if os.path.exists(cache_path):
                 return torch.load(cache_path)
         return None
+    
+    def cache_file(self, idx):
+        if self.cache_dir:
+            cache_path = os.path.join(self.cache_dir, f'{idx}.pt')
+            if not os.path.exists(cache_path):
+                cache_dir = os.path.dirname(cache_path)
+                os.makedirs(cache_dir, exist_ok=True)
+                torch.save(cache_path)
         
 
 class PartDataset(Dataset):
@@ -50,49 +85,25 @@ class PartDataset(Dataset):
         index_path: str, 
         data_path: str,  
         mode: str = 'train',
-        subset = 0,
-        subset_seed = 0,
-        val_pct = 0,
-        random_validation = False,
-        label_type: str = 'txt',
         part_options: dict = {},
-        implicit: bool = True
+        implicit: bool = True,
+        feature_options: PartFeatures = PartFeatures(),
+        implicit_options: dict = {}
     ):
         with open(index_path, 'r') as f:
             index = json.load(f)
         
         self.template = index['template']
-        self.label_template = index.get('label_template', None)
         ids = index.get(mode, index['train'])
-
-        self.label_type = index.get('label_type', label_type)
-        if self.label_type == 'txt':
-            self.read_labels = lambda f: [x.strip() for x in f.readlines()]
-        elif self.label_type == 'pickle':
-            self.read_labels = lambda f: pickle.load(f)
-        else:
-            pass
-
-        # Reduce to a subset
-        if subset > 0:
-            if subset < 1: # Percentage Subset
-
-                pass
-            else: # Fixed Size Subset
-                random.seed(subset_seed)
-                ids = random.sample(ids, subset)
-        
-        # Create a validation set
-        if random_validation and val_pct > 0 and mode != "train":
-            random.shuffle(ids)
-        if mode=='train':
-            ids = [x for i,x in enumerate(ids) if i % 100 <= (100 - val_pct)]
-        elif mode=='validate':
-            ids = [x for i,x in enumerate(ids) if i % 100 > (100 - val_pct)]
-
         self.ids = ids
+        if 'train_labels' in index:
+            self.labels = index.get(f'{mode}_labels', index['train_labels'])
+            assert(len(ids) == len(self.labels))
         self.data_path = data_path
         self.part_options = part_options
+        self.implicit = implicit
+        self.feature_options = feature_options
+        self.implicit_options = implicit_options
 
     def get_options_struct(self):
         options = PartOptions()
@@ -106,9 +117,6 @@ class PartDataset(Dataset):
 
     def __getitem__(self, idx):
         model_path = self.template.format(*self.ids[idx])
-        label_path = self.label_template.format(*self.ids[idx]) if self.label_template else None
-        part_options = self.get_options_struct()
-        labels = [self.ids[idx][0]]
         if self.data_path.endswith('.zip'): # Read from Zip
             if not self.datafile:
                 self.datafile = ZipFile(self.data_path)
@@ -117,33 +125,21 @@ class PartDataset(Dataset):
                 if self.implicit:
                     model = ImplicitPart(model_data, **self.part_options)
                 else:
-                    model = Part(model_data, part_options)
-            if label_path:
-                with self.datafile.open(label_path, 'r') as f:
-                    labels = self.read_labels(f)
+                    model = Part(model_data, self.get_options_struct())
         else: # Read from directory
             model_path = os.path.join(self.data_path, model_path)
-            label_path = os.path.join(self.data_path, label_path) if label_path else None
-            model = Part(model_path, part_options)
-            if label_path:
-                with open(label_path, 'r') as f:
-                    labels = self.read_labels(f)
-        return model, labels
+            if self.implicit:
+                model = ImplicitPart(model_path, **self.part_options)
+            else:
+                model = Part(model_path, self.get_options_struct())
+        
+        if self.implicit:
+            graph = implicit_part_to_data(model, **self.implicit_options)
+        else:
+            graph = part_to_graph(model, self.feature_options)
+        
+        if hasattr(self, 'labels'):
+            graph.labels = torch.tensor(self.labels[idx]).long()
 
-class ImplicitPartDataset(PartDataset):
-    def __init__(self, 
-        index_path: str, 
-        data_path: str,  
-        mode: str = 'train',
-        subset = 0,
-        subset_seed = 0,
-        val_pct = 0,
-        random_validation = False,
-        label_type: str = 'txt',
-        part_options: dict = {}
-    ):
-        super().__init__(
-            index_path, data_path, mode, subset, subset_seed, 
-            val_pct, random_validation, label_type, part_options
-        )
+        return graph
 
