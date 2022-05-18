a1b3bdad68f4ec2421ef35507545fa79dab632e0
Ben Jones <bentodjones@gmail.com>
2022-05-17 22:33:01 -0700

    Added sbgcn_baseline.

--- transfer_learning/sbgcn_baseline.py
+++ transfer_learning/sbgcn_baseline.py
@@ -1,6 +1,11 @@
 from pytorch_lightning import LightningModule
 from torch.nn.functional import cross_entropy
 from torchmetrics import Accuracy
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+import os
 
 from automate import PartFeatures, SBGCN
 from automate.sbgcn import LinearBlock
@@ -92,9 +97,9 @@ class SBGCNBaseline(LightningModule):
         targets = batch.labels
         loss = cross_entropy(scores, targets)
         batch_size = len(targets)
-        self.log('loss_train', loss, batch_size=batch_size, on_step=True, on_epoch=True)
+        self.log('train_loss', loss, batch_size=batch_size, on_step=True, on_epoch=True)
         self.train_acc(preds, targets)
-        self.log('train_acc')
+        self.log('train_acc', self.train_acc, batch_size, on_step=True, on_epoch=True)
         return loss
     def validation_step(self, batch, batch_idx):
         scores = self(batch)
@@ -102,5 +107,56 @@ class SBGCNBaseline(LightningModule):
         targets = batch.labels
         loss = cross_entropy(scores, targets)
         batch_size = len(targets)
-        self.log(loss, 'train_loss', batch_size=batch_size, on_step=True, on_epoch=True)
-        self.train_acc(preds, targets)
+        self.log(loss, 'val_loss', batch_size=batch_size, on_step=True, on_epoch=True)
+        self.val_acc(preds, targets)
+        self.log('val_acc', self.val_acc, batch_size, on_step=False, on_epoch=True)
+    def test_step(self, batch, batch_idx):
+        scores = self(batch)
+        preds = scores.argmax(dim=1)
+        targets = batch.labels
+        batch_size = len(targets)
+        self.test_acc(preds, targets)
+        self.log('test_acc', self.test_acc, batch_size, on_step=False, on_epoch=True)
+
+def run_experiments(
+    index_path, 
+    ds_path, 
+    experiment_sizes, 
+    seeds, 
+    cache_dir,
+    log_dir,
+    val_frac=0.2, 
+    batch_size=32,):
+    
+    ds_name = os.path.basename(index_path).split('.')[0]
+    
+    part_options, feature_options, sbgcn_options = automate_options()
+    
+
+    for seed in seeds:
+        for exp_size in experiment_sizes:
+            datamodule = BRepDataModule(
+                index_path,
+                ds_path,
+                part_options=part_options,
+                implicit=False,
+                feature_options=feature_options,
+                val_frac=val_frac,
+                seed = seed,
+                batch_size=batch_size,
+                train_size=exp_size,
+                cache_dir=cache_dir,
+                memcache=True
+            )
+            exp_name = f'{ds_name}_sbgcn_{exp_size}_{seed}'
+            model = SBGCNBaseline(sbgcn_options, 64)
+            callbacks = [
+                    #EarlyStopping(monitor='val_loss', mode='min', patience=100),
+                    ModelCheckpoint(monitor='val_loss', save_top_k=1, filename="{epoch}-{val_loss:.6f}",mode="min"),
+                ]
+            logger = TensorBoardLogger(log_dir, exp_name)
+            trainer = pl.Trainer(max_epochs=-1, logger=logger, callbacks=callbacks, gpus=1)
+            trainer.fit(model,datamodule)
+            results = trainer.test(datamodule=datamodule)
+            test_accuracy = results[0]['test_acc']
+            
